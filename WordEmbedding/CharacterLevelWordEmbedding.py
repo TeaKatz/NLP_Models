077970b9d1e0b999454cc1537ead2e8167e87fd5
Panuthep <falcon_270394@hotmail.co.th>
2021-07-29 02:33:07 +0700

    Added RNN, GRU, LSTM CharacterLevelWordEmbedding

--- WordEmbedding/CharacterLevelWordEmbedding.py
+++ WordEmbedding/CharacterLevelWordEmbedding.py
@@ -1,6 +1,6 @@
 import torch
 
-from torch.nn import Module, Embedding
+from torch.nn import Module, Embedding, RNN, GRU, LSTM
 
 
 class CharacterLevelWordSparseEncoding(Module):
@@ -50,9 +50,9 @@ class CharacterLevelWordEmbedding(Module):
         super().__init__()
         self.mode = mode
 
-        self.l_embedding = Embedding(num_embeddings=num_embeddings, 
-                                     embedding_dim=embedding_dim, 
-                                     padding_idx=padding_idx)
+        self.embedding = Embedding(num_embeddings=num_embeddings, 
+                                   embedding_dim=embedding_dim, 
+                                   padding_idx=padding_idx)
 
     def forward(self, token_ids):
         """
@@ -63,7 +63,7 @@ class CharacterLevelWordEmbedding(Module):
                     (batch_size, words_num, word_length, embedding_dim) otherwise
         """
         # (batch_size, words_num, word_length, embedding_dim)
-        word_vecs = self.l_embedding(token_ids)
+        word_vecs = self.embedding(token_ids)
 
         if self.mode == "sum":
             # (batch_size, words_num, embedding_dim)
@@ -134,13 +134,13 @@ class PositionalCharacterLevelWordEmbedding(Module):
         super().__init__()
         self.mode = mode
 
-        self.l_word_embedding = Embedding(num_embeddings=num_embeddings, 
-                                          embedding_dim=embedding_dim, 
-                                          padding_idx=padding_idx)
+        self.word_embedding = Embedding(num_embeddings=num_embeddings, 
+                                        embedding_dim=embedding_dim, 
+                                        padding_idx=padding_idx)
 
-        self.l_pos_embedding = Embedding(num_embeddings=max_positional,
-                                         embedding_dim=embedding_dim,
-                                         padding_idx=padding_idx)
+        self.pos_embedding = Embedding(num_embeddings=max_positional,
+                                       embedding_dim=embedding_dim,
+                                       padding_idx=padding_idx)
 
     def forward(self, token_ids, position_ids):
         """
@@ -152,7 +152,7 @@ class PositionalCharacterLevelWordEmbedding(Module):
                     (batch_size, words_num, word_length, embedding_dim) otherwise
         """
         # (batch_size, words_num, word_length, embedding_dim)
-        word_vecs = self.l_word_embedding(token_ids) + self.l_pos_embedding(position_ids)
+        word_vecs = self.word_embedding(token_ids) + self.pos_embedding(position_ids)
 
         if self.mode == "sum":
             # (batch_size, words_num, embedding_dim)
@@ -167,3 +167,97 @@ class PositionalCharacterLevelWordEmbedding(Module):
             # (batch_size, words_num, embedding_dim)
             word_vecs = torch.max(word_vecs, dim=2)[0]
         return word_vecs
+
+    
+class RNNCharacterLevelWordEmbedding(Module):
+    def __init__(self, num_embeddings, embedding_dim, padding_idx=0, num_layers=1):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.padding_idx = padding_idx
+        self.num_layers = num_layers
+
+        self.embedding = Embedding(num_embeddings=num_embeddings, 
+                                   embedding_dim=embedding_dim, 
+                                   padding_idx=padding_idx)
+
+        self.rnn = RNN(input_size=embedding_dim,
+                       hidden_size=embedding_dim,
+                       num_layers=num_layers,
+                       batch_first=True)
+
+    def forward(self, token_ids):
+        """
+        token_ids: (batch_size, words_num, word_length)
+
+        Return
+        word_vecs:  (batch_size, words_num, embedding_dim) if mode is ("sum", "mean", "max")
+                    (batch_size, words_num, word_length, embedding_dim) otherwise
+        """
+        batch_size, words_num, word_length = token_ids.shape
+
+        # (batch_size, words_num, word_length, embedding_dim)
+        word_vecs = self.embedding(token_ids)
+
+        # (1, batch_size, embedding_dim)
+        h0 = torch.zeros(1, batch_size * words_num, self.embedding_dim)
+        
+        # (batch_size * words_num, word_length, embedding_dim)
+        word_vecs = word_vecs.view(batch_size * words_num, word_length, -1)
+        word_vecs, _ = self.rnn(word_vecs, h0)
+
+        # (batch_size, words_num, word_length, embedding_dim)
+        word_vecs = word_vecs.view(batch_size, words_num, word_length, -1)
+        return word_vecs[:, :, -1]
+
+
+class GRUCharacterLevelWordEmbedding(RNNCharacterLevelWordEmbedding):
+    def __init__(self, num_embeddings, embedding_dim, padding_idx=0, num_layers=1):
+        super().__init__(num_embeddings, embedding_dim, padding_idx, num_layers)
+        self.rnn = GRU(input_size=embedding_dim,
+                       hidden_size=embedding_dim,
+                       num_layers=num_layers,
+                       batch_first=True)
+
+
+class LSTMCharacterLevelWordEmbedding(Module):
+    def __init__(self, num_embeddings, embedding_dim, padding_idx=0, num_layers=1):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.padding_idx = padding_idx
+        self.num_layers = num_layers
+
+        self.embedding = Embedding(num_embeddings=num_embeddings, 
+                                   embedding_dim=embedding_dim, 
+                                   padding_idx=padding_idx)
+
+        self.rnn = LSTM(input_size=embedding_dim,
+                        hidden_size=embedding_dim,
+                        num_layers=num_layers,
+                        batch_first=True)
+
+    def forward(self, token_ids):
+        """
+        token_ids: (batch_size, words_num, word_length)
+
+        Return
+        word_vecs:  (batch_size, words_num, embedding_dim) if mode is ("sum", "mean", "max")
+                    (batch_size, words_num, word_length, embedding_dim) otherwise
+        """
+        batch_size, words_num, word_length = token_ids.shape
+
+        # (batch_size, words_num, word_length, embedding_dim)
+        word_vecs = self.embedding(token_ids)
+
+        # (1, batch_size, embedding_dim)
+        h0 = torch.zeros(1, batch_size * words_num, self.embedding_dim)
+        c0 = torch.zeros(1, batch_size * words_num, self.embedding_dim)
+        
+        # (batch_size * words_num, word_length, embedding_dim)
+        word_vecs = word_vecs.view(batch_size * words_num, word_length, -1)
+        word_vecs, _ = self.rnn(word_vecs, (h0, c0))
+
+        # (batch_size, words_num, word_length, embedding_dim)
+        word_vecs = word_vecs.view(batch_size, words_num, word_length, -1)
+        return word_vecs[:, :, -1]
